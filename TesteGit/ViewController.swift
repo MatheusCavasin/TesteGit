2d1b27832d2b3229ede5d8ccb1874d60472ddab8
Matheus Cavasin <matheus.scavasin@gmail.com>
2019-04-24 09:24:15 -0300

    teste

--- TesteGit/ViewController.swift
+++ TesteGit/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Abre a primeira tela")
+        print("Abre a segunda tela")
     }
     
 
